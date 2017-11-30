24bc9caf5e3d24e78d70173af7c54545f8bf9f11
CloudZY <zy24@vip.qq.com>
2017-11-29 18:58:35 -0800

    2nd version of GRU neural network and add call procedures in Main

--- gru_nn.py
+++ gru_nn.py
@@ -1,41 +1,26 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
-class RNN_GRU(nn.Module):
+class GRU_Classifier(nn.Module):
 
     def __init__(self, vocabulary_size, embedding_dim, hidden_size, output_size, batch_size):
-        super(RNN_GRU, self).__init__()
+        super(GRU_Classifier, self).__init__()
         self.word_embeddings = nn.Embedding(vocabulary_size, embedding_dim)
         self.gru = nn.GRU(embedding_dim, hidden_size)
         self.linear = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(output_size)
+        self.softmax = nn.LogSoftmax()
+        self.embedding_dim = embedding_dim
         self.batch_size = batch_size
+        self.hidden = self.init_hidden(hidden_size)
 
-    def forward(self, sentence):
-        embeds = self.word_embeddings(sentence)
-        x = embeds.view(self.batch_size, len(sentence), -1)
-        h_gru = self.gru(x)
+    def forward(self, sentences):
+        embeds = self.word_embeddings(sentences)
+        x = embeds.view(len(sentences), -1, self.embedding_dim)
+        h_gru, self.hidden = self.gru(x, self.hidden)
         o_linear = self.linear(h_gru)
         y_predict = self.softmax(o_linear)
         return y_predict
 
-# batch_size, input_size, hidden_size, output_size = 32, 50, 200, 20
-#
-# x = Variable(torch.randn(batch_size, input_size))
-# y = Variable(torch.randn(batch_size, output_size), requires_grad=True)
-#
-# model = RNN_GRU(input_size, hidden_size, output_size)
-#
-# criterion = nn.NLLLoss()
-# optimizer = optim.SGD(model.parameters(), lr=1e-4)
-# epoch_num = 500
-#
-# for t in range(epoch_num):
-#     y_pred = model(x)
-#
-#     loss = criterion(y_pred, y)
-#     print(t, loss.data[0])
-#
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
+    def init_hidden(self, hidden_size):
+        return Variable(torch.randn(1, self.batch_size, hidden_size))
---
+++ main.py
@@ -0,0 +1,50 @@
+import numpy as np
+import utils
+from gru_nn import GRU_Classifier
+import torch.nn as nn
+import torch.optim as optim
+import torch
+from torch.autograd import Variable
+
+def main():
+    tweets, emojis = utils.load_data(max_example=100)
+    word_dict = utils.build_dict(tweets)
+    # embeddings = utils.generate_embeddings(word_dict, dim=50, pretrained_path='data/glove.twitter.27B.50d.txt')
+    embeddings = utils.generate_embeddings(word_dict, dim=50, pretrained_path=None)
+
+    x, y = utils.vectorize(tweets, emojis, word_dict)
+
+    all_train = utils.generate_batches(x,y,batch_size=32)
+
+    # set the parameters
+    batch_size, input_size, hidden_size, output_size = 32, 50, 200, 20
+    vocabulary_size = len(embeddings)
+
+    # initialize the model
+    model = GRU_Classifier(vocabulary_size, input_size, hidden_size, output_size, batch_size)
+    model.word_embeddings.weight.data = torch.from_numpy(embeddings)
+    loss_function = nn.NLLLoss()
+    optimizer = optim.SGD(model.parameters(), lr=1e-4)
+    epoch_num = 500
+
+    # model training
+    for epoch in range(epoch_num):
+        np.random.shuffle(all_train)
+        for idx, (mb_x, mb_y) in enumerate(all_train):
+            print('#Examples = %d, max_seq_len = %d' % (len(mb_x), mb_x.shape[1]))
+
+            mb_x = Variable(torch.from_numpy(np.array(mb_x, dtype=np.int64)))
+            y_pred = model(mb_x)
+            mb_y = Variable(torch.from_numpy(np.array(mb_y, dtype=np.int64)))
+            loss = loss_function(y_pred, torch.from_numpy(mb_y))
+            print('epoch ', epoch, 'batch ', idx, loss.data[0])
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print("----")
+
+
+if __name__ == '__main__':
+    main()
